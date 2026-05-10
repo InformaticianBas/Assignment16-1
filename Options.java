6a75be744247fd11b0f7000b54e1109293a3a81d
Bas M <cheminduloup@protonmail.com>
2026-05-10 12:09:19 -0400

    Fix Options

--- Options.java
+++ Options.java
@@ -27,7 +27,7 @@ public class Options {
     }
 
     public static void printHelp() {
-        System.out.println("Usuage: java [java options] VaporStore [option]");
+        System.out.println("Usage: java [java options] VaporStore [option]");
         System.out.println("Program options: \n \t -h for help \n \t -l to begin with login prompt ");
     }
 
